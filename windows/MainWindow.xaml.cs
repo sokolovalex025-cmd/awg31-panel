210193d721bd2111653c19b6a22f8d7a9a7fcd4d
sokolovalex025-cmd <sokolovalex025@gmail.com>
2026-09-09 09:49:10 +0300

    Fix Windows WebView2 initialization in code-behind

--- windows/MainWindow.xaml.cs
+++ windows/MainWindow.xaml.cs
@@ -1,15 +1,49 @@
 using System;
-using System.Diagnostics;
 using System.Windows;
+using Microsoft.Web.WebView2.Wpf;
+
 namespace AWGPanel.Desktop;
+
 public partial class MainWindow : Window
 {
-    public MainWindow(){InitializeComponent(); Loaded += async (_,__) => { await Browser.EnsureCoreWebView2Async(); Browser.CoreWebView2.NavigationCompleted += (_,__) => UpdateButtons(); Navigate(); };}
-    void Navigate(){ if(Uri.TryCreate(AddressBox.Text.Trim(), UriKind.Absolute, out var uri) && (uri.Scheme==Uri.UriSchemeHttp || uri.Scheme==Uri.UriSchemeHttps)) Browser.Source=uri; else MessageBox.Show("Укажите корректный HTTP/HTTPS адрес панели.","AWG Panel",MessageBoxButton.OK,MessageBoxImage.Warning); }
-    void UpdateButtons(){ BackButton.IsEnabled=Browser.CanGoBack; ForwardButton.IsEnabled=Browser.CanGoForward; AddressBox.Text=Browser.Source?.ToString() ?? AddressBox.Text; }
-    void Back_Click(object s,RoutedEventArgs e){if(Browser.CanGoBack)Browser.GoBack();}
-    void Forward_Click(object s,RoutedEventArgs e){if(Browser.CanGoForward)Browser.GoForward();}
-    void Reload_Click(object s,RoutedEventArgs e){Browser.Reload();}
-    void Open_Click(object s,RoutedEventArgs e){Navigate();}
-    void Settings_Click(object s,RoutedEventArgs e){MessageBox.Show("Здесь можно будет добавить профиль, адрес панели и дополнительные настройки.","AWG Panel",MessageBoxButton.OK,MessageBoxImage.Information);}
+    private readonly WebView2 Browser = new();
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        BrowserHost.Children.Add(Browser);
+        Loaded += async (_, _) =>
+        {
+            await Browser.EnsureCoreWebView2Async();
+            Browser.CoreWebView2.NavigationCompleted += (_, _) => UpdateButtons();
+            Navigate();
+        };
+    }
+
+    private void Navigate()
+    {
+        var text = AddressBox.Text.Trim();
+        if (!text.StartsWith("http://", StringComparison.OrdinalIgnoreCase) &&
+            !text.StartsWith("https://", StringComparison.OrdinalIgnoreCase))
+            text = "http://" + text;
+
+        if (Uri.TryCreate(text, UriKind.Absolute, out var uri) &&
+            (uri.Scheme == Uri.UriSchemeHttp || uri.Scheme == Uri.UriSchemeHttps))
+            Browser.Source = uri;
+        else
+            MessageBox.Show("Укажите корректный HTTP/HTTPS адрес панели.", "AWG Panel", MessageBoxButton.OK, MessageBoxImage.Warning);
+    }
+
+    private void UpdateButtons()
+    {
+        BackButton.IsEnabled = Browser.CanGoBack;
+        ForwardButton.IsEnabled = Browser.CanGoForward;
+        AddressBox.Text = Browser.Source?.ToString() ?? AddressBox.Text;
+    }
+
+    private void Back_Click(object sender, RoutedEventArgs e) { if (Browser.CanGoBack) Browser.GoBack(); }
+    private void Forward_Click(object sender, RoutedEventArgs e) { if (Browser.CanGoForward) Browser.GoForward(); }
+    private void Reload_Click(object sender, RoutedEventArgs e) => Browser.Reload();
+    private void Open_Click(object sender, RoutedEventArgs e) => Navigate();
+    private void Settings_Click(object sender, RoutedEventArgs e) => MessageBox.Show("Адрес панели можно изменить в строке сверху.", "AWG Panel", MessageBoxButton.OK, MessageBoxImage.Information);
 }
